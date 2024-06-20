3b96c882720ccb385e24402ad4a7b7fd87db75cb
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2024-06-21 00:37:06 +0200

    added game stats

---
+++ Assets/_Herdsman/Scripts/Stats/GameStats.cs
@@ -0,0 +1,17 @@
+﻿namespace Herdsman
+{
+    public class GameStats : IGameStats
+    {
+        public int Score { get; private set; }
+
+        public void AddScore(int additionalScore)
+        {
+            Score += additionalScore;
+        }
+
+        public void UpdateScore(int newScore)
+        {
+            Score = newScore;
+        }
+    }
+}
---
+++ Assets/_Herdsman/Scripts/Stats/IGameStats.cs
@@ -0,0 +1,9 @@
+﻿namespace Herdsman
+{
+    public interface IGameStats
+    {
+        int Score { get; }
+        void AddScore(int additionalScore);
+        void UpdateScore(int newScore);
+    }
+}
