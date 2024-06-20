978ca1731e46657967c11a5add72e9b3ce474dd1
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2024-06-21 00:36:53 +0200

    added basic score

---
+++ Assets/_Herdsman/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Herdsman
+{
+    public class ScoreUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _score;
+
+        // inject GameStats to get score?
+
+        private void Start()
+        {
+            
+        }
+
+        private void UpdateScore(int currentScore)
+        {
+            _score.text = currentScore.ToString();
+        }
+
+        private void OnDestroy()
+        {
+            
+        }
+    }
+}
