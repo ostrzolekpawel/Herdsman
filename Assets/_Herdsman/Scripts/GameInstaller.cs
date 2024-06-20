48f0e21504e789eb36977c010815a2eb1cff4520
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2024-06-21 00:37:59 +0200

    added game installer to inject dependencies

---
+++ Assets/_Herdsman/Scripts/GameInstaller.cs
@@ -0,0 +1,35 @@
+﻿using OsirisGames.EventBroker;
+using UnityEngine;
+using Herdsman.Animals;
+using Herdsman.Player;
+
+namespace Herdsman
+{
+    public class GameInstaller : MonoBehaviour
+    {
+        [SerializeField] private AnimalSpawner _animalSpawner;
+        [SerializeField] private HeroCreator _heroCreator;
+        [SerializeField] private MouseInputReader _inputReader;
+
+        [SerializeField] private AnimalSpawnerDataConfig _animalSpawnerData;
+        [SerializeField] private HerdDataConfig _herdDataConfig;
+
+        private IEventBus _signalBuys;
+
+        private void Awake()
+        {
+            _signalBuys = new EventBus();
+            var herd = new Herd(_herdDataConfig);
+
+            _animalSpawner.Init(_signalBuys, _animalSpawnerData);
+            _heroCreator.Init(_signalBuys, herd);
+            _inputReader.Init(_signalBuys);
+        }
+
+        private void Update()
+        {
+            _animalSpawner.Tick();
+            _heroCreator.Tick();
+        }
+    }
+}
