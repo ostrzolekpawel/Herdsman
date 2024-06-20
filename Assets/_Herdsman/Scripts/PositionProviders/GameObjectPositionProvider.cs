c256ae6a816be6041f71fc1082efc151805b0548
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2024-06-21 00:36:14 +0200

    gameobject position provider

---
+++ Assets/_Herdsman/Scripts/PositionProviders/GameObjectPositionProvider.cs
@@ -0,0 +1,20 @@
+﻿using Herdsman.PositionProviders;
+using UnityEngine;
+
+namespace Herdsman.PositionProviders
+{
+    public class GameObjectPositionProvider : IPositionProvider
+    {
+        private readonly Transform _transform;
+
+        public GameObjectPositionProvider(Transform transform)
+        {
+            _transform = transform;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return _transform.position;
+        }
+    }
+}
--- Assets/_Herdsman/Scripts/PositionProviders/RandomBoundSectorPositionProvider.cs
+++ Assets/_Herdsman/Scripts/PositionProviders/RandomBoundSectorPositionProvider.cs
@@ -22,24 +22,18 @@ namespace Herdsman.PositionProviders
 
         private void CalculatePossiblePositions()
         {
-            // calculate possible positions substracting restricted area from allowed area keeping in mind sector size
-
-            // Get the min and max points of the allowed area
             var allowedMin = _allowedAreaBounds.min;
             var allowedMax = _allowedAreaBounds.max;
 
-            // Get the min and max points of the restricted area
             var restrictedMin = _restrictedAreaBounds.min;
             var restrictedMax = _restrictedAreaBounds.max;
 
-            // Iterate over all positions within the allowed bounds
             for (int x = Mathf.FloorToInt(allowedMin.x / _sectorSize) * _sectorSize; x <= Mathf.FloorToInt(allowedMax.x / _sectorSize) * _sectorSize; x += _sectorSize)
             {
                 for (int y = Mathf.FloorToInt(allowedMin.y / _sectorSize) * _sectorSize; y <= Mathf.FloorToInt(allowedMax.y / _sectorSize) * _sectorSize; y += _sectorSize)
                 {
                     var position = new Vector2Int(x, y);
 
-                    // Check if the position is outside the restricted area
                     if (position.x < restrictedMin.x || position.x > restrictedMax.x ||
                         position.y < restrictedMin.y || position.y > restrictedMax.y)
                     {
@@ -53,7 +47,7 @@ namespace Herdsman.PositionProviders
         {
             return _possiblePositions.Count == 0 ? 
                 Vector2.zero : 
-                _possiblePositions[UnityEngine.Random.Range(0, _possiblePositions.Count)]; 
+                _possiblePositions[Random.Range(0, _possiblePositions.Count)]; 
         }
     }
 }
