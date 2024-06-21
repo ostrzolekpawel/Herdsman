8228acb8c98386e71ca9fc057e193dcc1cd7e4e8
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2024-06-21 23:26:26 +0200

    changes on scene and in game installer

--- Assets/_Herdsman/Scripts/GameInstaller.cs
+++ Assets/_Herdsman/Scripts/GameInstaller.cs
@@ -10,26 +10,33 @@ namespace Herdsman
         [SerializeField] private AnimalSpawner _animalSpawner;
         [SerializeField] private HeroCreator _heroCreator;
         [SerializeField] private MouseInputReader _inputReader;
+        [SerializeField] private GameStatsView _gameStatsView;
 
         [SerializeField] private AnimalSpawnerDataConfig _animalSpawnerData;
-        [SerializeField] private HerdDataConfig _herdDataConfig;
 
-        private IEventBus _signalBuys;
+        private IEventBus _signalBus;
+        private IGameStats _gameStats;
 
         private void Awake()
         {
-            _signalBuys = new EventBus();
-            var herd = new Herd(_herdDataConfig);
+            _signalBus = new EventBus();
+            _gameStats = new GameStats(_signalBus);
 
-            _animalSpawner.Init(_signalBuys, _animalSpawnerData);
-            _heroCreator.Init(_signalBuys, herd);
-            _inputReader.Init(_signalBuys);
+            _animalSpawner.Init(_signalBus, _animalSpawnerData);
+            _heroCreator.Init(_signalBus);
+            _inputReader.Init(_signalBus);
+            _gameStatsView.Init(_gameStats);
         }
 
         private void Update()
         {
             _animalSpawner.Tick();
             _heroCreator.Tick();
+        }
+
+        private void OnDestroy()
+        {
+            _gameStats.Dispose();
         }
     }
 }
